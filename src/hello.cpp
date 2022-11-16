e0c79610b2a58283c12c8df990afa29ac30a04fd
cjsdiniz <cjsdiniz@outlook.com>
2022-11-16 23:01:20 +0000

    Update hello.cpp

--- src/hello.cpp
+++ src/hello.cpp
@@ -3,7 +3,11 @@ using namespace std;
 
 int main()
 {
+  int num;
   cout << "Hello World"  << endl;
   cout << " This " << " is " << "awesome!";
+
+  cin >> num;
+  cout << "Num: " << num;
   return 0;
 }
