7898553f3edfb4e0f79c70775e10cc5c3bcab4fd
Carlos Diniz <67067863+cjsdiniz@users.noreply.github.com>
2022-11-29 22:07:02 +0000

    Several

---
+++ src/BankAccount.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class BankAccount{
+    public:
+        /* BankAccount();
+        void deposit(int amount);*/
+        void sayHi(){
+            cout << "Hi" << endl;
+        }
+};
+
+int main(){
+    
+        BankAccount test;
+        test.sayHi();
+        
+
+}
