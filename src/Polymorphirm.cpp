6981454182954025c3cd26f40988c27135107d6c
Carlos Diniz <67067863+cjsdiniz@users.noreply.github.com>
2022-12-14 12:26:31 +0000

    Polymorphism

---
+++ src/Polymorphirm.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Enemy {
+ protected: 
+  int attackPower;
+ public:
+  void setAttackPower(int a){
+   attackPower = a;
+  }
+};
+
+class Ninja: public Enemy {
+ public:
+  void attack() {
+   cout << "Ninja! - "<<attackPower<<endl;
+  }
+};
+
+class Monster: public Enemy {
+ public:
+  void attack() {
+   cout << "Monster! - "<<attackPower<<endl;
+  }
+};
+
+int main() {   
+ Ninja n;
+ Monster m;  
+}
