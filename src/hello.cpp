dbb1571465d726d2afe1fb9d3af2e4f2ebf8b606
Carlos Diniz <cjsdiniz@outlook.com>
2023-11-29 13:55:24 +0000

    Update hello.cpp

--- src/hello.cpp
+++ src/hello.cpp
@@ -6,8 +6,8 @@ int main()
   int num;
   cout << "Hello World"  << endl;
   cout << " This " << " is " << "awesome!";
-
-  cin >> num;
+  cout << "Num: \n";
+  cin >>  num;
   cout << "Num: " << num;
   return 0;
 }
